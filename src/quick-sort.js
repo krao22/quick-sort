20638241007aa50f3c7ce85809a83121d6374168
Kutumba Nunna <Kutumba_Nunna@epam.com>
2017-07-12 13:35:06 +0530

    quick sort implementation

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,12 @@
 'use strict';
 
 function quickSort(input) {
-
+	var resultArray = [];
+	if(input != null && input.length > 0)
+	{
+		resultArray = input.sort();
+	}
+	return resultArray;
 };
-module.exports = quickSort;
+var inputArray = [1,2];
+module.exports = quickSort(inputArray);
